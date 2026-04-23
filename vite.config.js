5db76a7b6ae00947735fd87b229c27362d89ac46
fahmijaafar <54757196+fahmijaafar@users.noreply.github.com>
2026-04-23 20:45:44 +0800

    revert entrypoint

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clore-ims-frontend/',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  base: '/',
+  plugins: [react()]
 })
