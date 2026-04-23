c7cfa641a2606fb0c1df7aa31ba424606e0b3e78
fahmijaafar <54757196+fahmijaafar@users.noreply.github.com>
2026-04-23 20:29:48 +0800

    added react plugin to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/clore-ims-frontend/'
+  base: '/clore-ims-frontend/',
+  plugins: [react()]
 })
